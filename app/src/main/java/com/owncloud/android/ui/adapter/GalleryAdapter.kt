bd82212abbcc19f7029e6d905fb7ef0c1dbc700b
tobiasKaminsky <tobias@kaminsky.me>
2022-04-06 11:45:30 +0200

    wip
    Signed-off-by: tobiasKaminsky <tobias@kaminsky.me>

--- app/src/main/java/com/owncloud/android/ui/adapter/GalleryAdapter.kt
+++ app/src/main/java/com/owncloud/android/ui/adapter/GalleryAdapter.kt
@@ -35,6 +35,7 @@ import com.owncloud.android.datamodel.FileDataStorageManager
 import com.owncloud.android.datamodel.GalleryItems
 import com.owncloud.android.datamodel.OCFile
 import com.owncloud.android.utils.DisplayUtils
+import com.owncloud.android.utils.FileStorageUtils
 import java.util.Calendar
 import java.util.Date
 
@@ -103,10 +104,9 @@ class GalleryAdapter(val context: Context) : SectionedRecyclerViewAdapter<Sectio
 
         files = items
             .groupBy { firstOfMonth(it.creationTimestamp) }
-            .map { GalleryItems(it.key, it.value) }
-            .sortedBy { it.date }.reversed()
-
-        //FileStorageUtils.sortOcFolderDescDateModifiedWithoutFavoritesFirst(mFiles)
+            .map { GalleryItems(it.key, FileStorageUtils.sortOcFolderDescDateModifiedWithoutFavoritesFirst(it.value)) }
+            .sortedBy { it.date }
+            .reversed()
 
         Handler(Looper.getMainLooper()).post { notifyDataSetChanged() }
     }
