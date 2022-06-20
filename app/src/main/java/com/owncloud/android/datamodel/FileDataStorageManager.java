57a9673e9e44a7079fb74ed1eff04d30392cd3ea
tobiasKaminsky <tobias@kaminsky.me>
2022-06-20 14:45:26 +0200

    Fix missing update of storage path, so that files/folders were only locally changed, but update not reflected in UI
    Signed-off-by: tobiasKaminsky <tobias@kaminsky.me>

--- app/src/main/java/com/owncloud/android/datamodel/FileDataStorageManager.java
+++ app/src/main/java/com/owncloud/android/datamodel/FileDataStorageManager.java
@@ -699,7 +699,14 @@ public class FileDataStorageManager {
                     contentValues.put(
                         ProviderTableMeta.FILE_PATH,
                         targetPath + childFile.getRemotePath().substring(lengthOfOldPath)
-                    );
+                                     );
+
+                    if (!childFile.isEncrypted()) {
+                        contentValues.put(
+                            ProviderTableMeta.FILE_PATH_DECRYPTED,
+                            targetPath + childFile.getRemotePath().substring(lengthOfOldPath)
+                                         );
+                    }
 
                     if (childFile.getStoragePath() != null && childFile.getStoragePath().startsWith(defaultSavePath)) {
                         // update link to downloaded content - but local move is not done here!
